a014c8ba31e9090c24a3cc6c9ec8cdcbb182c412
tmmoond8 <tmmoond8@gmail.com>
2022-02-12 23:20:56 +0900

    ✨ kakao map 렌더링

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 function App() {
-  return <div className="App">abc</div>;
+  return (
+    <Map
+      center={{ lat: 33.5563, lng: 126.79581 }}
+      style={{
+        position: 'fixed',
+        inset: '0',
+        margin: 'auto',
+      }}
+    >
+      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+        <div style={{ color: '#000' }}>Hello World!</div>
+      </MapMarker>
+    </Map>
+  );
 }
 
 export default App;
