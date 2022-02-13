409c98d344d5bf7bbb6b7113acf0ac69fd214c95
tmmoond8 <tmmoond8@gmail.com>
2022-02-13 23:20:04 +0900

    🚧 현재 위치 가져와서 셋팅하기

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,31 @@ import React from 'react';
 import { Map, MapMarker } from 'react-kakao-maps-sdk';
 
 function App() {
+  const [center, setCenter] = React.useState({
+    lat: 33.5563,
+    lng: 126.79581,
+  });
+  React.useEffect(() => {
+    const options = {
+      enableHighAccuracy: true,
+      timeout: 5000,
+      maximumAge: 0,
+    };
+    const success = ({ coords }: { coords: GeolocationCoordinates }) => {
+      console.log(`lat: ${coords.latitude}, long: ${coords.longitude}`);
+      setCenter({
+        lat: coords.latitude,
+        lng: coords.longitude,
+      });
+    };
+    const error = (error: GeolocationPositionError) => {
+      console.warn(error);
+    };
+    navigator.geolocation.getCurrentPosition(success, error, options);
+  }, []);
   return (
     <Map
-      center={{ lat: 33.5563, lng: 126.79581 }}
+      center={center}
       style={{
         position: 'fixed',
         inset: '0',
