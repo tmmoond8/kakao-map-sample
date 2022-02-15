2ae4a63500550fe95a9df57f389d060d290eee29
tmmoond8 <tmmoond8@gmail.com>
2022-02-16 01:09:34 +0900

    ✨ 타겟 지정하고, bounds 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
 import { Map, MapMarker, MapProps } from 'react-kakao-maps-sdk';
 import { css } from '@emotion/react';
+import { Button } from '@chakra-ui/react';
 import BounceLoader from 'react-spinners/BounceLoader';
 
-type Center = MapProps['center'];
+interface Point {
+  lat: number;
+  lng: number;
+}
+
+const loadingCss = css`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 function App() {
-  const [center, setCenter] = React.useState<Center | null>(null);
+  const mapRef = React.useRef<kakao.maps.Map>();
+  const [center, setCenter] = React.useState<Point | null>(null);
+  const [targetPoint, setTargetPoint] = React.useState<Point | null>(null);
+  const [level, setLevel] = React.useState<number>(3);
+
+  const bounds = React.useMemo(() => {
+    const bounds = new kakao.maps.LatLngBounds();
+    if (center) {
+      bounds.extend(new kakao.maps.LatLng(center.lat, center.lng));
+    }
+    if (targetPoint) {
+      bounds.extend(new kakao.maps.LatLng(targetPoint.lat, targetPoint.lng));
+    }
+    return bounds;
+  }, [center, targetPoint]);
 
   React.useEffect(() => {
     const options = {
@@ -27,12 +52,9 @@ function App() {
     navigator.geolocation.getCurrentPosition(success, error, options);
   }, []);
 
-  const loadingCss = css`
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-  `;
+  const handleChangeZoom = (map: kakao.maps.Map) => {
+    setLevel(map.getLevel());
+  };
 
   return (
     <>
@@ -44,11 +66,46 @@ function App() {
             inset: '0',
             margin: 'auto',
           }}
+          onCreate={(map: kakao.maps.Map) => (mapRef.current = map)}
+          onClick={(_map, event) => {
+            const lat = event.latLng?.getLat();
+            const lng = event.latLng?.getLng();
+            if (lat && lng) {
+              setTargetPoint({ lat, lng });
+            }
+          }}
+          onZoomChanged={handleChangeZoom}
+          maxLevel={12}
+          zoomable={true}
+          draggable={true}
         >
-          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-            <div style={{ color: '#000' }}>Hello World!</div>
+          <MapMarker position={center}>
+            <div style={{ color: '#000' }}>I 'm here</div>
           </MapMarker>
+          {targetPoint && (
+            <MapMarker position={targetPoint}>
+              <div style={{ color: '#000' }}>Target</div>
+            </MapMarker>
+          )}
         </Map>
+      )}
+      {targetPoint && (
+        <Button
+          position="fixed"
+          colorScheme="yellow"
+          w="200px"
+          bottom="40px"
+          left="0"
+          right="0"
+          margin="auto"
+          onClick={() => {
+            if (mapRef.current) {
+              mapRef.current.setBounds(bounds);
+            }
+          }}
+        >
+          setBounds
+        </Button>
       )}
       {!center && (
         <BounceLoader color="#fae100" loading css={loadingCss} size={150} />
