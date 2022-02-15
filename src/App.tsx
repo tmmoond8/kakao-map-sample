83735f16e21576200ec8fde610e25d0fee9645e3
tmmoond8 <tmmoond8@gmail.com>
2022-02-15 23:23:21 +0900

    ✨ 현재 위치로 지도 띄우고, 로딩 처리

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import { Map, MapMarker } from 'react-kakao-maps-sdk';
+import { Map, MapMarker, MapProps } from 'react-kakao-maps-sdk';
+import { css } from '@emotion/react';
+import BounceLoader from 'react-spinners/BounceLoader';
+
+type Center = MapProps['center'];
 
 function App() {
-  const [center, setCenter] = React.useState({
-    lat: 33.5563,
-    lng: 126.79581,
-  });
+  const [center, setCenter] = React.useState<Center | null>(null);
+
   React.useEffect(() => {
     const options = {
       enableHighAccuracy: true,
@@ -24,19 +26,34 @@ function App() {
     };
     navigator.geolocation.getCurrentPosition(success, error, options);
   }, []);
+
+  const loadingCss = css`
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  `;
+
   return (
-    <Map
-      center={center}
-      style={{
-        position: 'fixed',
-        inset: '0',
-        margin: 'auto',
-      }}
-    >
-      <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
-        <div style={{ color: '#000' }}>Hello World!</div>
-      </MapMarker>
-    </Map>
+    <>
+      {center && (
+        <Map
+          center={center}
+          style={{
+            position: 'fixed',
+            inset: '0',
+            margin: 'auto',
+          }}
+        >
+          <MapMarker position={{ lat: 33.55635, lng: 126.795841 }}>
+            <div style={{ color: '#000' }}>Hello World!</div>
+          </MapMarker>
+        </Map>
+      )}
+      {!center && (
+        <BounceLoader color="#fae100" loading css={loadingCss} size={150} />
+      )}
+    </>
   );
 }
 
